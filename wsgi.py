89ef604fb1ea28408c88baa69014b5328340cac7
Christian Briggs <briggzay@gmail.com>
2017-03-23 00:01:40 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,5 +3,5 @@ from V2App import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8081, debug=True)
+    app.run(host='0.0.0.0', port=8001, debug=True)
     #app.run()
